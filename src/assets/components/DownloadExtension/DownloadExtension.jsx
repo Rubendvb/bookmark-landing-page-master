173ea80d6168dcbfd8965d729358fe3b446b1651
Rubendvb <rubenvasquez556@gmail.com>
2023-09-10 02:24:00 -0300

    finalizando section downloadExtension

--- src/assets/components/DownloadExtension/DownloadExtension.jsx
+++ src/assets/components/DownloadExtension/DownloadExtension.jsx
@@ -3,37 +3,42 @@ import LogoFirefox from '../../images/logo-firefox.svg'
 import LogoOpera from '../../images/logo-opera.svg'
 import BgDots from '../../images/bg-dots.svg'
 
+import styles from './DownloadExtension.module.scss'
+
 export default function DownloadExtension() {
   return (
-    <section>
-      <h2>Download the extension</h2>
-      <p>
-        We’ve got more browsers in the pipeline. Please do let us know if you’ve
-        got a favorite you’d like us to prioritize.
-      </p>
+    <section className={styles.downloadExtension}>
       <div>
-        <article>
+        <h2 className={styles.title}>Download the extension</h2>
+        <p className={styles.text}>
+          We’ve got more browsers in the pipeline. Please do let us know if
+          you’ve got a favorite you’d like us to prioritize.
+        </p>
+      </div>
+
+      <div className={styles.containerBrowser}>
+        <article className={styles.card}>
           <img src={LogoChrome} alt="" />
-          <h3>Add to Chrome</h3>
-          <span>Minimum version 62</span>
+          <h3 className={styles.titleBrowser}>Add to Chrome</h3>
+          <span className={styles.span}>Minimum version 62</span>
           <img src={BgDots} alt="" />
-          <button>Add & Install Extension</button>
+          <button className={styles.button}>Add & Install Extension</button>
         </article>
 
-        <article>
+        <article className={styles.card}>
           <img src={LogoFirefox} alt="" />
-          <h3>Add to Firefox</h3>
-          <span>Minimum version 55</span>
+          <h3 className={styles.titleBrowser}>Add to Firefox</h3>
+          <span className={styles.span}>Minimum version 55</span>
           <img src={BgDots} alt="" />
-          <button>Add & Install Extension</button>
+          <button className={styles.button}>Add & Install Extension</button>
         </article>
 
-        <article>
+        <article className={styles.card}>
           <img src={LogoOpera} alt="" />
-          <h3>Add to Opera</h3>
-          <span>Minimum version 46</span>
+          <h3 className={styles.titleBrowser}>Add to Opera</h3>
+          <span className={styles.span}>Minimum version 46</span>
           <img src={BgDots} alt="" />
-          <button>Add & Install Extension</button>
+          <button className={styles.button}>Add & Install Extension</button>
         </article>
       </div>
     </section>
